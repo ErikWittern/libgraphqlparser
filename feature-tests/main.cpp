e3dd88b168f921087f1a2cb84384d3afcfeb1b67
Erik Wittern <witternj@us.ibm.com>
2019-07-11 14:00:53 -0400

    Add some feature tests

---
+++ feature-tests/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2018-present, IBM
+ *
+ * The purpose of this file is merely to provide a main function via the Catch2
+ * testing framework.
+ * 
+ * For details, refer to:
+ *   https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md
+ * 
+ * This code attempts to speed up compile times, as described here:
+ *   https://github.com/catchorg/Catch2/blob/master/docs/slow-compiles.md
+ */
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() function
+#include "catch.hpp"
