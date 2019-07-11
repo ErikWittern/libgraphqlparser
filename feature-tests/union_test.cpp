93ccfd85f6ce7bd9f25955f9df033c1777d78f09
Erik Wittern <witternj@us.ibm.com>
2019-07-11 16:58:34 -0400

    Add further feature tests

---
+++ feature-tests/union_test.cpp
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2019-present, IBM
+ *
+ * Build with:
+ * c++ -std=gnu++11 -I ./ feature-tests/*.cpp libgraphqlparser.dylib -o test.out
+ */
+#include <fstream>
+#include "Ast.h"
+#include "GraphQLParser.h"
+#include "c/GraphQLAstToJSON.h"
+#include "catch.hpp"
+
+using namespace std;
+using namespace facebook::graphql;  // use "parseString" without namespace
+using namespace facebook::graphql::ast;
+
+TEST_CASE("Union in schema", "[union]") {
+  SECTION("Read schema including description") {
+    FILE *fp = fopen("./feature-tests/union_schema.graphql", "r");
+    const char *error = nullptr;
+    auto AST = parseFileWithExperimentalSchemaSupport(fp, &error);
+
+    if (!AST) {
+      cerr << "Parser failed with error: " << error << endl;
+      free((void *)error);  // NOLINT
+    }
+    // auto schemaDocument = dynamic_cast<Document *>(AST.get());
+
+    const char *json = graphql_ast_to_json(
+        reinterpret_cast<const struct GraphQLAstNode *>(AST.get()));
+    puts(json);
+    free((void *)json);  // NOLINT
+
+    REQUIRE(AST != nullptr);
+  }
+}
